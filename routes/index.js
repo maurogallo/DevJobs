6cd04a81c53664e04133d586179817615723a99e
Mauricio <maurogallo@gmail.com>
2019-06-07 22:33:46 -0500

    Creando el formulario para nuevas vacantes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 const express = require ('express');
 const router =  express.Router();
 const homeControllers = require('../controllers/homeController');
+const vacantesController = require('../controllers/vacantesController');
 
 
 module.exports = () => {
     router.get('/', homeControllers.mostrarTrabajos);
+
+    //crear vacantes
+
+    router.get('/vacantes/nueva', vacantesController.formularioNuevaVacante);
 
     return router;
 }
