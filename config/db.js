9d6bf92f555bc0df64126d7cf8129cb9ea521650
Mauricio <maurogallo@gmail.com>
2019-06-07 20:33:36 -0500

    Definiendo el modelo de vacantes

--- config/db.js
+++ config/db.js
@@ -6,3 +6,7 @@ mongoose.connect(process.env.DATABASE, {useNewUrlParser:true});
 mongoose.connection.on('error', (error) => {
     console.log(error);
 })
+
+// importar los modeloes
+
+require('../models/vacantes');
